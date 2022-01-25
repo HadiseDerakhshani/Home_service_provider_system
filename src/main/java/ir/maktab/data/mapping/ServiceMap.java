47bef1ac84d8c43aa8b0386a9b4c82759dc2450b
HadiseDerakhshani <sevensama0@gmail.com>
2022-01-26 00:49:52 +0330

    modify createServiceDto method in ServiceMap class

--- src/main/java/ir/maktab/data/mapping/ServiceMap.java
+++ src/main/java/ir/maktab/data/mapping/ServiceMap.java
@@ -6,15 +6,23 @@ import ir.maktab.data.model.serviceSystem.Service;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 @Data
 @Component
-@RequiredArgsConstructor
+
 public class ServiceMap {
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
+    private  final SubServiceMap subServiceMap;
+@Autowired
+    public ServiceMap(ModelMapper mapper,@Lazy SubServiceMap subServiceMap) {
+        this.mapper = mapper;
+        this.subServiceMap = subServiceMap;
+    }
 
     public Service createService(ServiceDto serviceDto) {
         return mapper.map(serviceDto, Service.class);
@@ -24,10 +32,10 @@ public class ServiceMap {
         ServiceDto serviceDto = ServiceDto.builder()
                 .name(service.getName())
                 .build();
-        SubServiceMap subServiceMap = new SubServiceMap();
-        Set<SubServiceDto> collect = service.getSubServiceList().stream().map(subServiceMap::createSubServiceDto).collect(Collectors.toSet());
-        serviceDto.setSubServiceList(collect);
+      if(service.getSubServiceList().size()!=0){
+          serviceDto.setSubServiceList( service.getSubServiceList().stream().map(subServiceMap::createSubServiceDto)
+                .collect(Collectors.toSet()));
+    }
         return serviceDto;
-        // return mapper.map(service, ServiceDto.class);
     }
 }

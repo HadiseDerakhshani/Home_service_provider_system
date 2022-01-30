e804756d426368081a825fd3e6c405879c5d41df
HadiseDerakhshani <sevensama0@gmail.com>
2022-01-30 18:39:39 +0330

    add UserController class

--- src/main/java/ir/maktab/web/UserController.java
+++ src/main/java/ir/maktab/web/UserController.java
@@ -1,4 +1,16 @@
 package ir.maktab.web;
 
+import ir.maktab.data.dto.CustomerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+
 }

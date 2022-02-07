a3735b25632eabca874ed2554dd8dacfaeb4237e
HadiseDerakhshani <sevensama0@gmail.com>
2022-02-07 21:51:53 +0330

    add TransactionServiceImpl class in implemention package

---
+++ src/main/java/ir/maktab/service/implemention/TransactionServiceImpl.java
@@ -0,0 +1,13 @@
+package ir.maktab.service.implemention;
+
+import ir.maktab.service.TransactionService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+@RequiredArgsConstructor
+public class TransactionServiceImpl implements TransactionService {
+
+}
